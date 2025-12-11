be468e08e2029ba887a38a84a9b6827a9b4f1d56
Reggie Cushing <r.cushing@esciencecenter.nl>
2025-12-11 11:19:20 +0100

    File rename.

---
+++ examples/example_fetch_file_extensions.py
@@ -0,0 +1,19 @@
+import filefetcher as ff
+
+PIDS = ["10.17026/DANS-XGB-TW5U", "http://hdl.handle.net/21.T15999/01BYJvzYl"]
+
+def main():
+    try:
+        for pid in PIDS:
+            file_list = ff.file_extensions(pid)
+            file_mime_types = ff.file_mime_types(pid)
+            print(f"Fetched file extension and mime-types for PID {pid}:")
+            print(file_list)
+            print(file_mime_types)
+            print("-" * 40)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
+
